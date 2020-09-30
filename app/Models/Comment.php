a69252837dc79de40aee1bac98066c1cb5fd30a8
Jason McCreary <jason@pureconcepts.net>
2020-09-30 15:30:17 -0400

    Build some models with Blueprint

---
+++ app/Models/Comment.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+        'published_at',
+        'author_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'author_id' => 'integer',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'published_at',
+    ];
+
+
+    public function author()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+}
